27bf70574d15b970ba3f0035a207d8e56c6ee7e3
Sreekanth Ayydevara <sreekanth@bedynamic.com>
2016-06-02 02:46:45 -0700

    minor change

--- Services/TestService.cs
+++ Services/TestService.cs
@@ -15,7 +15,7 @@ namespace Services
     {
         public IEnumerable<string> GetTestValues()
         {
-            return new List<string> {"Hello", "this", "is", "a", "injected", "service"};
+            return new List<string> {"Hello", "this", "is", "a", "injected", "service", "method"};
         }
     }
 }
